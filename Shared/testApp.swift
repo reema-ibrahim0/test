f17d51251d9ed3f9b024976911a33207db81d3e5
reema <ribrahim6@wisc.edu>
2023-04-09 13:38:33 -0500

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by REEMA IBRAHIM on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
